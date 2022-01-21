e1049be13b4b5f9717ed2f6ab79003dfaaa64a02
6210743073 <poohmiphan@outlook.com>
2022-01-21 11:19:17 +0700

    'updateex'

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -3,7 +3,7 @@ import Product from "../product/Product";
 import { products } from "../../data";
 import RecommendIcon from "@mui/icons-material/Recommend";
 import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
-import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
 const ProductList = () => {
   return (
     <div className="pl">
@@ -25,8 +25,9 @@ const ProductList = () => {
           สัปดาห์ในการพัฒนา จากโปรเจคนี้ทำให้ผมได้พัฒนาทั้ง fonts end และ
           backend และทำให้ผมเริ่มชอบในการเป็น full stack developer
           <br></br>
-          <SettingsApplicationsIcon/>
+          <SettingsApplicationsIcon />
           กำลังพัฒนาเว็ปไซด์ร้านค้า
+          <br></br>
           <RecommendIcon />
           ประสบการณ์ในการออกแบบฐานข้อมูลโดยใช้ SQL และ PHP เขียนบน Laravel
           framework
